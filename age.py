f57df258b6e73f1e12ff66640516dad05693dc14
Touchimaru <111978620+Touchimaru@users.noreply.github.com>
2022-08-25 01:50:37 -0800

    Add files via upload

---
+++ age.py
@@ -0,0 +1,18 @@
+# python age calculator 
+print(" age calculator using python")
+
+#user input
+
+age = int(input("Enter the birth year : "))
+
+
+# the year when i made this program is 2022 i will change it when 2022 ends 
+year = 2022
+
+#the total age
+total = year - age
+
+print(" The age is " , total , " years old" )
+
+print("Thanks for using this program press enter to exit ")
+input()
